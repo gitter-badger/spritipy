a92ed372ee03974895575da8924d1aed8b0a0d0f
surchs <sebastian.urchs@gmail.com>
2018-01-11 17:33:15 -0500

    package changes

--- setup.py
+++ setup.py
@@ -6,14 +6,11 @@ with open('README.md', encoding='utf-8') as fd:
     LONG_DESCRIPTION = fd.read()
 
 setup(
-    name='brainsprite',
+    name='spritipy',
     long_description=LONG_DESCRIPTION,
     license='MIT',
-    packages=['brainsprite'],
-    package_data={
-        'assets': 'assets/*',
-        'examples': 'examples/*'
-    },
+    packages=['spritipy'],
+    url='https://github.com/brainsprite/spritipy',
     include_package_data=True,
     install_requires=[
         'matplotlib',
@@ -27,11 +24,6 @@ setup(
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Development Status :: 3 - Alpha',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
 )
